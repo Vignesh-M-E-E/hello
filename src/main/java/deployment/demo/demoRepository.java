d9d5b8fe337a9c019614aa1383a7c13644fb394b
VIGNESH M E <vigneshvikcy03@gmail.com>
2026-01-09 23:28:39 +0530

    Initial commit

---
+++ src/main/java/deployment/demo/demoRepository.java
@@ -0,0 +1,8 @@
+package deployment.demo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(path = "users")
+public interface demoRepository extends JpaRepository<User, Long> {
+}
